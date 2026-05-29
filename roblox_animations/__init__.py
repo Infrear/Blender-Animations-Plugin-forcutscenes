fa6436325281837c04c7aadafaa5e8245b1b08eb
⚠️ <76855302+Cautioned@users.noreply.github.com>
2026-05-29 00:36:42 -0400

    v2.6.3

--- roblox_animations/__init__.py
+++ roblox_animations/__init__.py
@@ -26,7 +26,7 @@ bl_info = {
     "name": "Roblox Animations Importer/Exporter",
     "description": "Plugin for importing roblox rigs and exporting animations.",
     "author": "Cautioned",
-    "version": (2, 6, 2),
+    "version": (2, 6, 3),
     "blender": (2, 80, 0),
     "location": "View3D > Toolbar",
 }
